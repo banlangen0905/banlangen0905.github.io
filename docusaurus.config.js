a7cd1f6abdedc04b655d00096c44ef3755d52707
zhujingli <banlangen_0905@qq.com>
2023-06-05 16:15:41 +0800

    测试修改

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '朱静丽',
+  title: '朱静丽干活的证明',
   tagline: 'Banlangen',
   favicon: 'img/favicon.ico',
 
